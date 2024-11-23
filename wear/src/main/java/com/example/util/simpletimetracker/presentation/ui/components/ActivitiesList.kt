465028366344c93ef741db7308be9516e2ed368e
razeeman <freeraz@mail.ru>
2024-11-23 09:12:51 +0300

    fix wear font scale issues

--- wear/src/main/java/com/example/util/simpletimetracker/presentation/ui/components/ActivitiesList.kt
+++ wear/src/main/java/com/example/util/simpletimetracker/presentation/ui/components/ActivitiesList.kt
@@ -8,16 +8,15 @@ package com.example.util.simpletimetracker.presentation.ui.components
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
@@ -71,11 +70,11 @@ fun ActivitiesList(
             is ActivitiesListState.Loading -> item {
                 RenderLoading()
             }
-            is ActivitiesListState.Error -> item {
-                RenderError(state, onRefresh)
+            is ActivitiesListState.Error -> {
+                renderError(state, onRefresh)
             }
-            is ActivitiesListState.Empty -> item {
-                RenderEmpty(state, onOpenOnPhone)
+            is ActivitiesListState.Empty -> {
+                renderEmpty(state, onOpenOnPhone)
             }
             is ActivitiesListState.Content -> {
                 renderContent(
@@ -96,40 +95,43 @@ private fun RenderLoading() {
     )
 }
 
-@Composable
-private fun RenderError(
+private fun ScalingLazyListScope.renderError(
     state: ActivitiesListState.Error,
     onRefresh: () -> Unit,
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
+    item {
         Icon(
             painter = painterResource(R.drawable.wear_connection_error),
             contentDescription = null,
         )
+    }
+    item {
         Text(
             text = getString(stringResId = state.messageResId),
-            modifier = Modifier.padding(8.dp),
+            modifier = Modifier.padding(horizontal = 8.dp),
             textAlign = TextAlign.Center,
         )
+    }
+    item {
         RefreshButton(onRefresh)
     }
 }
 
-@Composable
-private fun RenderEmpty(
+private fun ScalingLazyListScope.renderEmpty(
     state: ActivitiesListState.Empty,
     onOpenOnPhone: () -> Unit,
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
+    item {
+        Spacer(modifier = Modifier)
+    }
+    item {
         Text(
             text = getString(stringResId = state.messageResId),
-            modifier = Modifier.padding(8.dp),
+            modifier = Modifier.padding(horizontal = 8.dp),
             textAlign = TextAlign.Center,
         )
+    }
+    item {
         OpenOnPhoneButton(onOpenOnPhone)
     }
 }
